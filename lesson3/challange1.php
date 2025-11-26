5c1ebbaa69a946f4fdb14946dbf42ead87188e4a
Lumi-09 <hazirilum12@gmail.com>
2025-11-26 17:40:07 +0100

    lesson3

---
+++ lesson3/challange1.php
@@ -0,0 +1,10 @@
+<?php
+    $a = 50;
+    $b = 50;
+    if($a == $b){
+        echo "yes";
+    }
+    else{
+        echo "No";
+    }
+?>
---
+++ lesson3/if_or.php
@@ -0,0 +1,7 @@
+<?php
+    $age = 10;
+    if(($age < 15) || ($age < 5))
+        {
+        echo "You are teenager";
+    }
+?>     
